be4645889503e85ed09043c3c92f0cc55efad244
root <root@osota.com>
2021-03-31 17:15:21 -0600

    dependencies examples depends_on

--- main.tf
+++ main.tf
@@ -44,3 +44,38 @@ resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
 }
 
+resource "random_string" "bucket" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name     = "learn-gcp-${random_string.bucket.result}"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_compute_instance" "another_instance" {
+  depends_on = [google_storage_bucket.example_bucket]
+
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
